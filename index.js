8b405f62e7e73ad5b6ccb2f848c763af60eb2e43
wajeht <zombyard@gmail.com>
2020-11-13 05:17:01 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// secret stuff
+require("dotenv").config();
+
+const twilio = require("twilio");
+const schedule = require("node-schedule");
+
+// twilio credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+// phone numbers
+const tedsNumber = process.env.TEDS_NUMBER;
+const myTwillioNumber = process.env.MY_NUMBER;
+
+// create twilio object
+const client = new twilio(accountSid, authToken);
+
+// create message
+const message = (message) => {
+    client.messages
+        .create({
+            body: message,
+            to: tedsNumber,
+            from: myTwillioNumber,
+        })
+        .then((message) => console.log(message.body));
+};
+
+// set chase date
+const chaseDate = new schedule.RecurrenceRule();
+chaseDate.date = 30;
+
+// set citi date
+const citiDate = new schedule.RecurrenceRule();
+citiDate.date = 14;
+
+// schedule cahse
+const chaseBill = schedule.scheduleJob(chaseDate, function () {
+    message("Your chase bill is up!");
+});
+
+// schedule citi
+const citiBill = schedule.scheduleJob(citiDate, function () {
+    message("Your citi bill is up!");
+});
